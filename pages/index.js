a24a78834ba4346dc95a5b8b9946a79b852146c7
fayehuang <faye1821@gmail.com>
2021-05-07 01:08:27 +0800

    -ADD: send talk api

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,18 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const handleClick = () => {
+    const url = `https://embed-chatbot.vercel.app/api/talk?appId=abc&userInput=HIHI`
+    fetch(url)
+    .then(res => res.json())
+    .catch(error => {
+      alert(`Error:\n${error}`)
+    })
+    .then(response => {
+      alert(`Success:\n${JSON.stringify(response, null, 2)}`)
+    });
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +24,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to Website2 !
         </h1>
+        <button onClick={handleClick} style={{fontSize:24, padding:8, backgroundColor:'aquamarine'}}>TALK</button>
       </main>
     </div>
   )
